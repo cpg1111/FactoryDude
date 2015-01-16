25ad6f6498df947664875ec6bad4fe8c8a793035
Christian Grabowski <christiang11754@gmail.com>
2015-01-16 01:03:32 -0800

    fix tests

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-require('mongoose');
-
 module.exports = function(relativePathToModel, newObject){
-	if(typeof newObject == 'object'){
+	if(typeof newObject != 'object'){
 		throw new Error('FactoryDude can only take objects to create new mongoose entries');
 		return;
-	}
-	if(!(relativePathToModel[0] == '.' && relativePathToModel[0] == '/')){
-		relativePathToModel = '.'+relativePathToModel;
 	}
 	var model = require(relativePathToModel);
 	var constructor = function(){
