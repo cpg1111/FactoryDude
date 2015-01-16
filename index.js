a6aed2a568158ed555cb7a9a861bf21dc3522af7
Christian Grabowski <christiang11754@gmail.com>
2015-01-15 23:29:57 -0800

    create FactoryDude module

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 require('mongoose');
 
-module.exports = function(newObject){
-
+module.exports = function(relativePathToModel, newObject){
+	if(typeof newObject == 'object'){
+		throw new Error('FactoryDude can only take objects to create new mongoose entries');
+		return;
+	}
+	if(!(relativePathToModel[0] == '.' && relativePathToModel[0] == '/')){
+		relativePathToModel = '.'+relativePathToModel;
+	}
+	var model = require(relativePathToModel);
+	var constructor = function(){
+		this.newModel = new model();
+		var attrCounter = 0;
+		for(var attr in newObject){
+			eval('this.newModel.'+attr+' = newObject.'+attr);
+		}
+		this.newModel.save(function(err){
+			if(err){
+				throw err;
+			}
+		});
+	};
+	return constructor().newModel;
 };
